71cf1d5114e73a7480fd2d0a5af258293af44fb8
duykasama <duykasama@gmail.com>
2023-09-16 14:01:25 +0700

    fetch apartments from api

--- src/pages/Apartments.jsx
+++ src/pages/Apartments.jsx
@@ -1,8 +1,10 @@
 import GridView from "../components/GridView";
 import AddNewApartment from "../components/modal/AddNewApartment";
+import useApartments from "../hooks/useApartments";
 
-function Apartments({ apartments }) {
-  const fields = ["Address", "RentalPrice", "NumberOfRooms"];
+function Apartments() {
+  const fields = ["address", "rentalPrice", "numberOfRooms"];
+  const { apartments } = useApartments();
 
   return (
     <div className="w-full h-full p-8 flex justify-center items-center">
