d4905cf0c8eaa4185a6a6358fc044cbe8a02b67c
duykasama <duykasama@gmail.com>
2023-09-16 13:57:27 +0700

    fetch contracts from api

--- src/pages/Contracts.jsx
+++ src/pages/Contracts.jsx
@@ -1,8 +1,10 @@
 import GridView from "../components/GridView";
 import AddNewContract from "../components/modal/AddNewContract";
+import useContracts from "../hooks/useContracts";
 
-function Contracts({ contracts }) {
-  const fields = ["ID", "CustomerID", "ApartmentID", "StartDate", "EndDate"];
+function Contracts() {
+  const fields = ["customerId", "apartmentId", "startDate", "endDate"];
+  const { contracts } = useContracts();
 
   return (
     <div className="w-full h-full p-8 flex justify-center items-center">
